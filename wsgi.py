06e0339b79a52242c5fb47dea00dc52637d6ff08
Giovanni Toffoli <toffoli@linkroma.it>
2015-12-28 12:13:43 +0100

    Fixed pythonpath in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# see: http://stackoverflow.com/questions/10752031/django-1-4-with-apache-virtualhost-path-problems
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print sys.path
+print path
+if path not in sys.path:
+    sys.path.append(path)
+path = '/home/ubuntu/fv/lib/python2.7/site-packages'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
