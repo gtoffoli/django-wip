11fe72466127449b7e8741120a01bccb3d7402f5
Giovanni Toffoli <toffoli@linkroma.it>
2016-04-11 18:02:05 +0200

    Fixed ProxyMiddleware class

--- middleware.py
+++ middleware.py
@@ -1,9 +1,25 @@
 from models import Proxy
 
+FORWARDED_FOR_FIELDS = [
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_FORWARDED_HOST',
+        'HTTP_X_FORWARDED_SERVER',
+    ]
+
 class ProxyMiddleware(object):
 
     def process_request(self, request):
-        host = request.META.get('HTTP_HOST', ''),
+        """
+        Rewrites the proxy headers so that only the most
+        recent proxy is used.
+        """
+        for field in FORWARDED_FOR_FIELDS:
+            if field in request.META:
+                if ',' in request.META[field]:
+                    parts = request.META[field].split(',')
+                    request.META[field] = parts[-1].strip()
+                    print request.META[field]
+        host = request.META.get('HTTP_HOST', '')
         print host, request.path_info
         for proxy in Proxy.objects.all():
             if proxy.host == host:
